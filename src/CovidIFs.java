2be06afca5aba08167627740d407fe77b12572fe
saleemsiddique <saleemprogramacion04@gmail.com>
2023-05-14 20:04:59 +0200

    Segunda clase terminada.

---
+++ src/CovidIFs.java
@@ -0,0 +1,35 @@
+public class CovidIFs {
+    public static void main(String[] args) {
+        // Ejemplo de prueba
+        float bodyTemperature = 38.5f;
+        boolean difficultyBreathing = false;
+        boolean diabetes = false;
+        boolean cancer = false;
+        boolean isPregnant = false;
+        boolean isOver60yearsold = false;
+        boolean hepatic = false;
+        boolean kidneyDisease = false;
+        boolean respiratoryDisease = false;
+        String province = "ABC";
+
+        String result = covid(bodyTemperature, difficultyBreathing, diabetes, cancer, isPregnant,
+                isOver60yearsold, hepatic, kidneyDisease, respiratoryDisease, province);
+
+        System.out.println("Resultado: " + result);
+    }
+    public static String covid(float bodyTemperature, boolean difficultyBreathing, boolean diabetes, boolean cancer, boolean isPregnant, boolean isOver60yearsold, boolean hepatic, boolean kidneyDisease, boolean respiratoryDisease, String province) {
+        boolean isHighTemperature = bodyTemperature >= 38;
+        boolean otherSymptoms = diabetes || cancer || isPregnant || isOver60yearsold || hepatic ||
+                kidneyDisease || respiratoryDisease;
+
+        if (isHighTemperature &&
+           (difficultyBreathing && otherSymptoms) ||
+                otherSymptoms ) {
+            return "/diagnostico/" + province;
+        } else if (isHighTemperature) {
+            return "/cuarentena/";
+        } else {
+            return "/diagnostico_bueno/";
+        }
+    }
+}
